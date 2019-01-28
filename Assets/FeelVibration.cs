9717de8b7c78ea3ebf8763ee9d60ef1dd412a9a6
Tsutomu Yamaguchi <t.yamaguchi.air@outlook.jp>
2019-01-28 18:58:18 +0900

    first sample
    二本の棒をjoyconで動かせるよ

---
+++ Assets/FeelVibration.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+public class Direct : MonoBehaviour
+{
+    private List<Joycon> m_joycons;
+    private Joycon m_joyconL;
+
+    private void Start()
+    {
+        m_joycons = JoyconManager.Instance.j;
+
+        if (m_joycons == null || m_joycons.Count <= 0) return;
+
+        m_joyconL = m_joycons.Find(c => c.isLeft);
+    }
+
+    private void Update()
+    {
+        if (m_joycons == null || m_joycons.Count <= 0) return;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            StartCoroutine("Forward");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine("Back");
+        }
+    }
+
+    IEnumerator Forward()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            m_joyconL.SetRumble(0, -500.0f, 1.0f, 20);
+            yield return new WaitForSeconds(0.02f);
+            m_joyconL.SetRumble(0, 1.0f, 0.2f, 50);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    IEnumerator Back()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            m_joyconL.SetRumble(0, 500.0f, 1.0f, 20);
+            yield return new WaitForSeconds(0.02f);
+            m_joyconL.SetRumble(0, -1.0f, 0.2f, 50);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+
+    private void OnGUI()
+    {
+        var style = GUI.skin.GetStyle("label");
+        style.fontSize = 24;
+
+        if (m_joycons == null || m_joycons.Count <= 0)
+        {
+            GUILayout.Label("Joy-Con が接続されていません");
+            return;
+        }
+
+        if (!m_joycons.Any(c => c.isLeft))
+        {
+            GUILayout.Label("Joy-Con (L) が接続されていません");
+            return;
+        }
+    }
+}
---
+++ Assets/MoveBox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBox : MonoBehaviour {
+
+    private List<Joycon> joycons;
+    private Joycon joyconL;
+    private Joycon joyconR;
+
+    public GameObject redcube;
+    public GameObject bluecube;
+
+    public Vector3 gyro;
+    public Vector3 accel;
+    public Quaternion orientation_L;
+    public Quaternion orientation_R;
+
+	// Use this for initialization
+	void Start () {
+        redcube = GameObject.Find("RedCube");
+        bluecube = GameObject.Find("BlueCube");
+        gyro = new Vector3(0, 0, 0);
+        accel = new Vector3(0, 0, 0);
+
+        joycons = JoyconManager.Instance.j;
+
+        if (joycons == null || joycons.Count <= 0) return;
+
+        joyconL = joycons.Find(c => c.isLeft);
+        joyconR = joycons.Find(c => !c.isLeft);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        bluecube.transform.rotation = new Quaternion(joyconL.GetVector().x,joyconL.GetVector().z,joyconL.GetVector().y,-joyconL.GetVector().w);
+        redcube.transform.rotation = new Quaternion(joyconR.GetVector().x,joyconR.GetVector().z,joyconR.GetVector().y,-joyconR.GetVector().w);;
+
+		
+	}
+}
